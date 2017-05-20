ad51b5079742a5da8f221d87bda761e1e2a823ac
Leif Andersen <leif@leifandersen.net>
2017-05-20 10:10:52 -0400

    Parse picture and GOB headers.

--- render.rkt
+++ render.rkt
@@ -20,7 +20,81 @@
 ;; All I guarentee at the moment is that it doesn't make video crash on installation.
 ;; Hopefully with time this will get moved out of the experimental section.
 
-(require bitsyntax)
+(require bitsyntax
+         racket/file
+         (for-syntax racket/base
+                     racket/syntax
+                     syntax/parse))
 
-(define PSC (integer->bit-string 20 #b00000000000000010000 #t))
-(define GBSC (integer->bit-string 16 #b0000000000000001 #t))
+(define-syntax (define-constant stx)
+  (syntax-parse stx
+    [(_ name:id number:nat width:nat)
+     #'(define-syntax name
+         (syntax-rules ()
+           [(_ #t input ks kf)
+            (bit-string-case input
+              ([(= number :: bits width) (rest :: binary)]
+               (ks number rest))
+              [else (kf)])]
+           [(_ #f src)
+            (number->bit-string number width #t)]))]))
+(define-syntax (define-header stx)
+  (syntax-parse stx
+    [(_ name:id width:nat)
+     #`(define-syntax name
+         (syntax-rules ()
+           [(_ #t input ks kf)
+            (bit-string-case input
+              ([(body :: integer bits width) (rest :: binary)]
+               (ks body rest))
+              (else (kf)))]
+           [(_ #f)
+            (bit-string width (str :: binary))]))]))
+
+
+
+(define-constant PSC #b00000000000000010000 20)
+(define-header TR 5)
+(define-header PTYPE 6)
+(define-header PEI 1)
+(define-header PSPARE 8)
+
+(define-constant GBSC #b0000000000000001 16)
+(define-header GN 4)
+(define-header GQUANT 5)
+(define-header GEI 1)
+(define-header GSPARE 8)
+
+(define (read-frame str)
+  (bit-string-case str
+    ([(:: (PSC))
+      (tr :: (TR))
+      (ptype :: (PTYPE))
+      (pei :: (PEI))
+      (rest :: binary)]
+     (if (= pei 1)
+         (read-pspare rest)
+         (read-gob rest)))))
+
+(define (read-pspare str)
+  (bit-string-case str
+    ([(pspare :: (PSPARE))
+      (pei :: (PEI))
+      (rest :: binary)]
+     (if (= pei 1)
+         (read-pspare rest)
+         (read-gob rest)))))
+
+(define (read-gob str)
+  (bit-string-case str
+    ([(gbsc :: (GBSC))
+      (gn :: (GN))
+      (gquant :: (GQUANT))
+      (rest :: binary)]
+     (displayln "yay"))))
+
+(define file
+  (file->bytes "/Users/leif/demo.h261"))
+
+(read-frame file)
+
