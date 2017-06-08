3af8331b48b205264eb3162199e9ad75fc5654a9
Leif Andersen <leif@leifandersen.net>
2017-06-08 15:05:16 -0400

    Use the new ffmpeg-stream api.

--- ffmpeg.rkt
+++ ffmpeg.rkt
@@ -6,13 +6,16 @@
          ffi/vector
          ffi/cvector
          data/gvector
+         racket/match
          opengl
          opengl/util
          portaudio
          video/private/ffmpeg
+         video/private/ffmpeg-stream
          video/private/audioqueue
          video/private/video-canvas)
 
+#|
 (define audio-decode-frame
   (let ()
     (define frame (av-frame-alloc))
@@ -91,48 +94,85 @@
     (cond [codec (values codec index)]
           [(equal? (avcodec-context-codec-type* c) type) (values c i*)]
           [else (values codec index)])))
+|#
 
-(define testfile "/Users/leif/demo2.mp4")
-(av-register-all)
-(define avformat (avformat-open-input testfile #f #f))
-(avformat-find-stream-info avformat #f)
-;(av-dump-format avformat 0 testfile 0)
-(define strs (avformat-context-streams avformat))
-(define-values (codec-ctx codec-index) (get-codec 'video))
-(define-values (audio-codec-ctx audio-codec-index) (get-codec 'audio))
-(define codec-id (avcodec-context-codec-id codec-ctx))
-(define audio-codec-id (avcodec-context-codec-id audio-codec-ctx))
-(define codec (avcodec-find-decoder codec-id))
-(define audio-codec (avcodec-find-decoder audio-codec-id))
-(define new-ctx (avcodec-copy-context codec codec-ctx))
-(define audio-new-ctx (avcodec-copy-context audio-codec audio-codec-ctx))
-(avcodec-open2 new-ctx codec #f)
-(avcodec-open2 audio-new-ctx audio-codec #f)
 (define frame (av-frame-alloc))
 (define frame-rgb (av-frame-alloc))
-(define num-bytes (av-image-get-buffer-size 'rgb24
-                                            (avcodec-context-width new-ctx)
-                                            (avcodec-context-height new-ctx)
-                                            32))
-(define buff (av-malloc num-bytes _uint8))
-(void
- (av-image-fill-arrays (av-frame-data frame-rgb)
-                       (av-frame-linesize frame-rgb)
-                 (array-ptr buff)
-                 'rgb24
-                 (avcodec-context-width new-ctx)
-                 (avcodec-context-height new-ctx)
-                 1))
-(define sws
-  (sws-getContext (avcodec-context-width new-ctx)
-                  (avcodec-context-height new-ctx)
-                  (avcodec-context-pix-fmt new-ctx)
-                  (avcodec-context-width new-ctx)
-                  (avcodec-context-height new-ctx)
-                  'rgb24
-                  SWS-BILINEAR
-                  #f #f #f))
+(define num-bytes #f)
+(define buff #f)
+(define sws #f)
+(define f #f)
+(define c #f)
+(stream-file
+ "/Users/leif/demo2.mp4"
+ #:video-callback (λ (mode obj packet)
+                    (match obj
+                      [(struct* codec-obj
+                                ([codec-context codec-context]))
+                       (match mode
+                         ['init
+                          (set! num-bytes
+                                (av-image-get-buffer-size'rgb24
+                                                         (avcodec-context-width codec-context)
+                                                         (avcodec-context-height codec-context)
+                                                         32))
+                          (set! buff (av-malloc num-bytes _uint8))
+                          (av-image-fill-arrays (av-frame-data frame-rgb)
+                                                (av-frame-linesize frame-rgb)
+                                                (array-ptr buff)
+                                                'rgb24
+                                                (avcodec-context-width codec-context)
+                                                (avcodec-context-height codec-context)
+                                                1)
+                          (set! sws
+                                (sws-getContext (avcodec-context-width codec-context)
+                                                (avcodec-context-height codec-context)
+                                                (avcodec-context-pix-fmt codec-context)
+                                                (avcodec-context-width codec-context)
+                                                (avcodec-context-height codec-context)
+                                                'rgb24
+                                                SWS-BILINEAR
+                                                #f #f #f))
+                          
+                          (set! f (new frame%
+                                       [label "Movie"]
+                                       [width (avcodec-context-width codec-context)]
+                                       [height (avcodec-context-height codec-context)]))
+                          (set! c (new video-canvas%
+                                       [width (avcodec-context-width codec-context)]
+                                       [height (avcodec-context-height codec-context)]
+                                       [parent f]))
+                          (send f show #t)]
+                         ['loop
+                          (with-handlers ([exn:ffmpeg:again? void]
+                                          [exn:ffmpeg:eof? void])
+                            (avcodec-send-packet codec-context packet)
+                            (avcodec-receive-frame codec-context frame)
+                            (sws-scale sws
+                                       (array-ptr (av-frame-data frame))
+                                       (array-ptr (av-frame-linesize frame))
+                                       0
+                                       (avcodec-context-height codec-context)
+                                       (array-ptr (av-frame-data frame-rgb))
+                                       (array-ptr (av-frame-linesize frame-rgb)))
+                            (define linesize (array-ref (av-frame-linesize frame-rgb) 0))
+                            (send c draw-frame
+                                  (λ ()
+                                    (for ([i (in-range (avcodec-context-height codec-context))])
+                                      (glTexSubImage2D GL_TEXTURE_2D
+                                                       0
+                                                       0
+                                                       i
+                                                       (avcodec-context-width codec-context)
+                                                       1
+                                                       GL_RGB
+                                                       GL_UNSIGNED_BYTE
+                                                       (ptr-add (array-ref (av-frame-data frame-rgb) 0)
+                                                                (* i linesize)))))))
+                          (av-packet-unref packet)]
+                         ['close (send f show #f)])])))
 
+#|
 (define swr (swr-alloc))
 (av-opt-set-int swr "in_channel_layout" (avcodec-context-channel-layout audio-new-ctx) 0)
 (av-opt-set-int swr "out_channel_layout" (cast 'sterio _av-ch _int) 0)
@@ -142,22 +182,14 @@
 (av-opt-set-sample-fmt swr "out_sample_fmt" 's16 0)
 (swr-init swr)
 
-(define f (new frame%
-               [label "Movie"]
-               [width (avcodec-context-width new-ctx)]
-               [height (avcodec-context-height new-ctx)]))
-(define c (new video-canvas%
-               [width (avcodec-context-width new-ctx)]
-               [height (avcodec-context-height new-ctx)]
-               [parent f]))
-(send f show #t)
-
 
 (define audio-queue (mk-audioqueue))
 
 (define audio-procs
   (stream-play/unsafe audio-callback 0.2 (avcodec-context-sample-rate audio-new-ctx)))
+|#
 
+#|
 (define film (make-gvector #:capacity 10000))
 (let loop ()
   (define packet (av-read-frame avformat))
@@ -166,42 +198,12 @@
                     [exn:ffmpeg:eof? void])
       (cond
         [(= (avpacket-stream-index packet) codec-index)
-         (avcodec-send-packet new-ctx packet)
-         (avcodec-receive-frame new-ctx frame)
-         
-         (sws-scale sws
-                    (array-ptr (av-frame-data frame))
-                    (array-ptr (av-frame-linesize frame))
-                    0
-                    (avcodec-context-height new-ctx)
-                    (array-ptr (av-frame-data frame-rgb))
-                    (array-ptr (av-frame-linesize frame-rgb)))
-         (define linesize (array-ref (av-frame-linesize frame-rgb) 0))
-         (send c draw-frame
-               (λ ()
-                 (for ([i (in-range (avcodec-context-height new-ctx))])
-                   (glTexSubImage2D GL_TEXTURE_2D
-                                    0
-                                    0
-                                    i
-                                    (avcodec-context-width new-ctx)
-                                    1
-                                    GL_RGB
-                                    GL_UNSIGNED_BYTE
-                                    (ptr-add (array-ref (av-frame-data frame-rgb) 0)
-                                             (* i linesize))))))
-         (av-packet-unref packet)]
         [(= (avpacket-stream-index packet) audio-codec-index)
          (audioqueue-put audio-queue packet)]
         [else
          (av-packet-unref packet)]))
     (loop)))
+|#
 
 (av-frame-free frame)
 (av-frame-free frame-rgb)
-(avcodec-close codec-ctx)
-(avcodec-close new-ctx)
-(avcodec-close audio-codec-ctx)
-(avcodec-close audio-new-ctx)
-(avformat-close-input avformat)
-(send f show #f)
