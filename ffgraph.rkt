9efad4016aced0715a711ab47bc0afedfa0dd4c3
Leif Andersen <leif@leifandersen.net>
2017-06-21 13:12:13 -0400

    Need an intermediate node.

--- ffgraph.rkt
+++ ffgraph.rkt
@@ -3,8 +3,10 @@
 (require video/private/video
          (only-in video/private/ffmpeg-pipeline
                   render
+                  mk-filter
+                  mk-filter-node
                   mk-sink-node
-                  bundle-for-file)
+                  bundle->file)
          graph)
 
 (current-render-graph (weighted-graph/directed '()))
@@ -15,9 +17,16 @@
 (define source-node
   (convert file))
 
+(define filter-node
+  (mk-filter-node
+   (hash 'video (mk-filter "copy")
+         'audio (mk-filter "anull"))))
+(add-vertex! (current-render-graph) filter-node)
+(add-directed-edge! (current-render-graph) source-node filter-node)
+
 (define sink-node
-  (mk-sink-node (bundle-for-file "/Users/leif/test.mp4" 'vid+aud)))
+  (mk-sink-node (bundle->file "/Users/leif/test.mp4" 'vid+aud)))
 (add-vertex! (current-render-graph) sink-node)
-(add-edge! (current-render-graph) source-node sink-node)
+(add-directed-edge! (current-render-graph) filter-node sink-node)
 
 (render (current-render-graph))
