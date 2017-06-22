1995f1ab99ebb5f48afb9bf7d79beaf45850a4f7
Leif Andersen <leif@leifandersen.net>
2017-06-22 14:12:07 -0400

    Woops, need to also set the frame rate.

--- ffgraph.rkt
+++ ffgraph.rkt
@@ -17,16 +17,24 @@
 (define source-node
   (convert file))
 
-(define filter-node
+(define filter-node-a
   (mk-filter-node
-   (hash 'video (mk-filter "copy")
+   (hash 'video (mk-filter "pad"
+                           #:args (hash "width" 1920
+                                        "height" 1080))
          'audio (mk-filter "anull"))))
-(add-vertex! (current-render-graph) filter-node)
-(add-directed-edge! (current-render-graph) source-node filter-node)
+(add-vertex! (current-render-graph) filter-node-a)
+(add-directed-edge! (current-render-graph) source-node filter-node-a)
+(define filter-node-b
+  (mk-filter-node
+   (hash 'video (mk-filter "fps"
+                           #:args (hash "fps" 25)))))
+(add-vertex! (current-render-graph) filter-node-b)
+(add-directed-edge! (current-render-graph) filter-node-a filter-node-b)
 
 (define sink-node
   (mk-sink-node (stream-bundle->file "/Users/leif/test.mp4" 'vid+aud)))
 (add-vertex! (current-render-graph) sink-node)
-(add-directed-edge! (current-render-graph) filter-node sink-node)
+(add-directed-edge! (current-render-graph) filter-node-b sink-node)
 
 (render (current-render-graph))
