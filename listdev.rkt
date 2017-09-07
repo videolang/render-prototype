e7cf28fee44d9a43c9fff94ab52f34e0ddf9e8da
Leif Andersen <leif@leifandersen.net>
2017-09-07 14:53:51 +0100

    Working prototype (for os x)

--- listdev.rkt
+++ listdev.rkt
@@ -1,19 +1,14 @@
 #lang racket
 
 (require video/private/ffmpeg/main
+         video/private/ffmpeg-pipeline
          video/private/init)
 
 (define fmt (av-find-input-format "avfoundation"))
 ;(set-av-input-format-flags!
 ; fmt (set-remove (av-input-format-flags fmt) 'nofile))
 (define ctx (avformat-alloc-context))
-(define avio (avio-alloc-context
-              (av-malloc 4096) ;; XXX Need to free!
-              4096
-              #t
-              #f #f #f #f))
 ;(set-avformat-context-iformat! ctx fmt)
-(set-avformat-context-pb! ctx avio)
-(avformat-open-input ctx "" fmt #f)
+(avformat-open-input ctx "" fmt (build-av-dict (hash "list_devices" "true")))
 (avformat-find-stream-info ctx #f)
 (avdevice-list-devices ctx)
