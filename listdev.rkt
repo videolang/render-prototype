545bd35d31ef12082a5a33d9449e9a3e828aef21
Leif Andersen <leif@leifandersen.net>
2017-09-07 20:22:31 +0100

    Can now output a nice device list.

--- listdev.rkt
+++ listdev.rkt
@@ -5,22 +5,43 @@
          video/private/ffmpeg-pipeline
          video/private/init)
 
+(struct device-list (video
+                     audio)
+  #:transparent)
+
 (define video-devices-str "AVFoundation video devices:")
 (define audio-devices-str "AVFoundation audio devices:")
-(define dev-regexp #rx"\\[0-9\\]*(.*)")
+(define dev-regexp #rx"\\[[0-9]*\\] (.*)")
 
-(flush-ffmpeg-log!)
-(with-intercepted-logging
-    (λ (l)
-      (match l
-        [(vector level message data topic)
-         (displayln message)]))
-  (λ ()
-    (define fmt (av-find-input-format "avfoundation"))
-    (define ctx (avformat-alloc-context))
-    (with-handlers ([exn? (λ (e) (void))])
-      (avformat-open-input ctx "" fmt (build-av-dict (hash "list_devices" "true"))))
-    (flush-ffmpeg-log!))
-  #:logger ffmpeg-logger
-  'info
-  (string->symbol "AVFoundation input device"))
+(define (list-input-devices)
+  (define curr-list (box #f))
+  (define video-list (box '()))
+  (define audio-list (box '()))
+  
+  (flush-ffmpeg-log!)
+  (with-intercepted-logging
+      (λ (l)
+        (match l
+          [(vector level message data topic)
+           (match message
+             [(regexp video-devices-str)
+              (set-box! curr-list video-list)]
+             [(regexp audio-devices-str)
+              (set-box! curr-list audio-list)]
+             [_
+              (define dev-name (cadr (regexp-match dev-regexp message)))
+              (set-box! (unbox curr-list)
+                        (cons dev-name (unbox (unbox curr-list))))])]))
+    (λ ()
+      (define fmt (av-find-input-format "avfoundation"))
+      (define ctx (avformat-alloc-context))
+      (with-handlers ([exn? (λ (e) (void))])
+        (avformat-open-input ctx "" fmt (build-av-dict (hash "list_devices" "true"))))
+      (flush-ffmpeg-log!))
+    #:logger ffmpeg-logger
+    'info
+    (string->symbol "AVFoundation input device"))
+  (device-list (reverse (unbox video-list))
+               (reverse (unbox audio-list))))
+
+(list-input-devices)
