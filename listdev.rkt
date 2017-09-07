59041197f68b54950fb00c21b7a30b7865c0a3ab
Leif Andersen <leif@leifandersen.net>
2017-09-07 14:56:10 +0100

    Much shorter prototype

--- listdev.rkt
+++ listdev.rkt
@@ -5,10 +5,8 @@
          video/private/init)
 
 (define fmt (av-find-input-format "avfoundation"))
-;(set-av-input-format-flags!
-; fmt (set-remove (av-input-format-flags fmt) 'nofile))
 (define ctx (avformat-alloc-context))
-;(set-avformat-context-iformat! ctx fmt)
-(avformat-open-input ctx "" fmt (build-av-dict (hash "list_devices" "true")))
-(avformat-find-stream-info ctx #f)
-(avdevice-list-devices ctx)
+(with-handlers ([exn? (λ (e) (void))])
+  (avformat-open-input ctx "" fmt (build-av-dict (hash "list_devices" "true"))))
+
+(sleep 3)
